0e9e06f57d3d450a5b097000142a77697cef1748
Mohammed SEBBAR <msebbar@sqli.com>
2024-04-22 18:52:19 +0200

    solve conflict in HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(private readonly LoggerInterface $logger){
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $this->logger->info('MESSAGE_FROM_SYMFONY', ['username' => 'user Clone', 'who_is' => 'e-challenger']);
+
         return $this->render('home/index.html.twig');
     }
 }
