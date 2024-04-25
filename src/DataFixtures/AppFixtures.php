1c573a967cc5b723a5064d4a089a652ab2a32753
yboudlal <yboudlal@sqli.com>
2024-04-25 18:33:34 +0200

    ajout de la partie d'authentification

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,18 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private const COUNT_USES=5;
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
-
-        $manager->flush();
+        UserFactory::createMany(self::COUNT_USES);
+         $manager->flush();
     }
 }
