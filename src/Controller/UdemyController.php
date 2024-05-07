6a22a4a39ef823f8b997093ab608242f5fd33cc5
yboudlal <yboudlal@sqli.com>
2024-05-07 16:31:17 +0200

    l'ajout des variable d'envionements dans les fichier .env .env.dev

--- src/Controller/UdemyController.php
+++ src/Controller/UdemyController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\SerializedCourse;
 use App\Service\UdemyApiClient;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class UdemyController extends AbstractController
 {
@@ -19,5 +24,40 @@ class UdemyController extends AbstractController
             'courses' => $courses['results'],
         ]);
     }
+    #[Route('/udemy/add', name: 'app_add_cours')]
+    public function addCours(Request $request,HttpClientInterface $httpClient,SerializerInterface $serializer, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UdemyType::class);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+
+            $id = $data['id'];
+
+
+            $response = $httpClient->request('GET', 'https://www.udemy.com/api-2.0/courses/'.$id);
+            $courseData = $response->toArray();
+            $serializedCourseData = $serializer->serialize($courseData, 'json');
+
+
+            $serializedCourse = new SerializedCourse();
+            $serializedCourse->setSerializedData($serializedCourseData);
+
+
+            $entityManager->persist($serializedCourse);
+            $entityManager->flush();
+
+
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('udemy/add_cours.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    }
 }
