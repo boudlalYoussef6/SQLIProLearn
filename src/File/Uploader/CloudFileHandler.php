bf5d0efb50b8bcca04f85c338896722aba0d3375
Issam KHADIRI <khadiri.issam@gmail.com>
2024-06-16 20:07:55 +0100

    command to delete orphan files from cloud

--- src/File/Uploader/CloudFileHandler.php
+++ src/File/Uploader/CloudFileHandler.php
@@ -6,6 +6,7 @@ namespace App\File\Uploader;
 
 use League\Flysystem\FilesystemException;
 use League\Flysystem\FilesystemOperator;
+use League\Flysystem\StorageAttributes;
 use Symfony\Component\DependencyInjection\Attribute\Target;
 
 final class CloudFileHandler implements FileHandlerInterface
@@ -40,5 +41,17 @@ final class CloudFileHandler implements FileHandlerInterface
         } catch (FilesystemException $e) {
             return '';
         }
+    }
+
+    /**
+     * @throws FilesystemException
+     */
+    public function listContent(string $path = '/'): array
+    {
+        return $this->storage
+            ->listContents($path)
+            ->filter(fn (StorageAttributes $attr) => $attr->isFile())
+            ->map(fn (StorageAttributes $attr) => $attr->path())
+            ->toArray();
     }
 }
